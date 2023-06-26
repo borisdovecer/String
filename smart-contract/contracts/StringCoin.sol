40a22608e1caf3ad9f03c40d2f8c9487b17f8011
borisdovecer <36776986+borisdovecer@users.noreply.github.com>
2023-06-26 20:29:45 +0200

    strc

--- smart-contract/contracts/StringCoin.sol
+++ smart-contract/contracts/StringCoin.sol
@@ -4,19 +4,12 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./StringStake.sol";
 
 contract StringCoin is ERC20, Pausable, Ownable {
-    uint256 public constant MAX_SUPPLY = 420000000 * (10 ** 18); // 420 million tokens, 18 decimal places
-    StringStake public _stakeString;
+    uint256 public constant MAX_SUPPLY = 420000000 * (10 ** 18);
 
-    constructor() ERC20("String Coin", "STRC") {
+    constructor() ERC20("String Coin", "STRC") Ownable() {
         _mint(msg.sender, MAX_SUPPLY);
-    }
-
-    function setStakingContract(address _stringStake) public  {
-        _stakeString = StringStake(_stringStake);
-        _approve(msg.sender, _stringStake, MAX_SUPPLY);
     }
 
     function pause() public onlyOwner {
